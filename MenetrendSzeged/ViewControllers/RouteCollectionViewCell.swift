17e2a4e30153d1c7ef5afe57907638ae62440e2c
Olivér Tóth <oli92facebook@gmail.com>
2018-06-04 22:15:44 +0200

    Adding routeCollectionView

---
+++ MenetrendSzeged/ViewControllers/RouteCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  RouteCollectionViewCell.swift
+//  MenetrendSzeged
+//
+//  Created by Toth Oliver on 6/4/18.
+//  Copyright © 2018 deXol. All rights reserved.
+//
+
+import UIKit
+
+class RouteCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet var routeName: UILabel!
+    
+}
