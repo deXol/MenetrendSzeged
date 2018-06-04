565030ba881ce7fb841e4c699e7f68cad04bf50f
Olivér Tóth <oli92facebook@gmail.com>
2018-06-04 22:19:09 +0200

    Test commit on developRouteView branch

--- MenetrendSzeged/ViewControllers/RouteCollectionViewController.swift
+++ MenetrendSzeged/ViewControllers/RouteCollectionViewController.swift
@@ -44,13 +44,11 @@ class RouteCollectionViewController: UICollectionViewController  {
     // MARK: UICollectionViewDataSource
 
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
 
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of items
         return routes.count
     }
 
